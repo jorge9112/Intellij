0528dbe8506c7b1bdd056d3fa29ad8e7cb9a91d0
Prueba <a_herrerajol@globalhitss.com>
2020-12-10 17:00:08 -0600

    prueba intellij

--- src/taudemo/SeleniumTest.java
+++ src/taudemo/SeleniumTest.java
@@ -1,4 +1,36 @@
 package taudemo;
 
+import browser.BrowserGetter;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import org.junit.jupiter.api.TestIntance;
+
+
 public class SeleniumTest {
-}
+    private BrowserGetter browserGetter = new BrowserGetter();
+    private WebDriver driver;
+
+
+    @BeforeAll
+    public void beforeAll() {
+        driver = browserGetter.getChromeDriver();
+
+    }
+    @AfterAll
+    public void afterAll(){
+        driver.quit();
+    }
+
+    @Test
+    public void openThePageAndCheckTitle() {
+        String expectedTitle = "Example title";
+        driver.get("https://www.example.com");
+        assertEquals(expectedTitle, driver.getTitle());
+    }
+
+
+    }
